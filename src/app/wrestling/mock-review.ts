3ebc1bd1949fb27a805ab4a0bea294dc5410801a
Chris <chris.tramier97@gmail.com>
2018-12-11 12:04:54 +0100

    edit Review class

--- src/app/wrestling/mock-review.ts
+++ src/app/wrestling/mock-review.ts
@@ -5,18 +5,24 @@ export const REVIEWS: Review[] = [
 		id:1,
 		name: "Survivor Series - Daniel Bryan vs Brock Lesnar",
 		picture: "http://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",
-		date: new Date()
+		date: new Date(),
+		year: null,
+		type: "WWE"
 	},
 	{
 		id:2,
 		name: "Survivor Series - Charlotte Flair vs Ronda Rousey",
 		picture: "http://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",
-		date: new Date()
+		date: new Date(),
+		year: null,
+		type: "4starslist"
 	},
 	{
 		id:3,
 		name: "Les 4* 2018",
 		picture: "http://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",
-		date: new Date()
+		date: new Date(),
+		year: null,
+		type: "4starslist"
 	},
 ];
--- src/app/wrestling/review.ts
+++ src/app/wrestling/review.ts
@@ -1,12 +1,18 @@
 export class Review {
     id: number;
     name: string;
-    picture: string; //Inutile pour une première version ?
+    picture: string; //Inutile pour une première version ? -> Peut être récupéré depuis le "type"
     date: Date;
+    year: number; //Utilisé pour le trie mais surtout les reviews qui ne concernent pas un show précis. 
+                  //TODO A supprimer et générer auto dans le futur.
+    type: string; //Type de review, peut être une fed ou une zone géographique
+                  //ex : Jap, PWG, UK, WWE, WWE NXT, WWE 205 Live, PROGRESS...
     //drivelink: string;
     //year: number;
   }
 
+
+  //Inutilisé pour le moment, à voir comment je recup toutes mes reviews (trop pour ranger dans des db...)
   export class Event extends Review {
     id: number;
     name: string;
