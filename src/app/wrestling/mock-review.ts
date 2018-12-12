bfecfc0557e04ff50b2ce1cab6878f2b076da40f
Chris <chris.tramier97@gmail.com>
2018-12-12 13:14:27 +0100

    Finalizing setup to migrate db

--- src/app/wrestling/mock-review.ts
+++ src/app/wrestling/mock-review.ts
@@ -22,7 +22,7 @@ export const REVIEWS: Review[] = [
 		name: "Les 4* 2018",
 		date: new Date(),
 		year: null,
-		driveLink:"https://docs.google.com/document/d/1Kcpzcil3QecaY4JGnD8FYBt8OS-qsQfBICFQlgWj-2c/edit?usp=sharing",
+		driveLink:"https://docs.google.com/document/d/1Kcpzcil3QecaY4JGnD8FYBt8OS-qsQfBICFQlgWj-2c/edit?usp=sharings",
 		type: "4starslist"
 	},
 ];
