1d6dd856ecd45d3569988fecda869b4213a47501
Chris <chris.tramier97@gmail.com>
2018-12-16 13:40:09 +0100

    mock completion

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,6 +1,7 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { POKEMONS } from './pokemons/mock-pokemon';
 import { REVIEWS } from './wrestling/mock-review';
+//utilisation du mock à nouveau, le node js c'est bien mais pas sur un serveur mutualisé...
 
 export class InMemoryDataService implements InMemoryDbService {
 	createDb() {
--- src/app/wrestling/mock-review.ts
+++ src/app/wrestling/mock-review.ts
@@ -1,28 +1,101 @@
 import { Review } from './review';
-
+//utilisation du mock à nouveau, le node js c'est bien mais pas sur un serveur mutualisé...
 export const REVIEWS: Review[] = [
+
 	{
 		id:1,
-		name: "Survivor Series - Daniel Bryan vs Brock Lesnar",
-		date: new Date(),
-		year: null,
-		driveLink:null,
-		type: "WWE"
-	},
-	{
-		id:2,
-		name: "Survivor Series - Charlotte Flair vs Ronda Rousey",
-		date: new Date(),
-		year: null,
-		driveLink:null,
-		type: "WWE"
-	},
-	{
-		id:3,
 		name: "Les 4* 2018",
 		date: new Date(),
 		year: null,
 		driveLink:"https://docs.google.com/document/d/1Kcpzcil3QecaY4JGnD8FYBt8OS-qsQfBICFQlgWj-2c/edit?usp=sharings",
+		type: "4starslist"
+	},
+	{
+		id:2,
+		name: "NJPW BOSJ 2018",
+		date: new Date(),
+		year: null,
+		driveLink:"https://docs.google.com/document/d/1310NlpB9UMBDdNG_Bgd_VgP9QHCRpoM9dSynxuPDXAA/edit",
+		type: "NJPW"
+	},
+	{
+		id:3,
+		name: "NJPW 2018",
+		date: new Date(),
+		year: null,
+		driveLink:"https://docs.google.com/document/d/1WlZ_5zcflboPzuKFzMWMruOetdFdbRRuo6S0Sy4WrAg/edit",
+		type: "NJPW"
+	},
+	{
+		id:4,
+		name: "NJPW G1 2018",
+		date: new Date(),
+		year: null,
+		driveLink:"https://docs.google.com/document/d/1KoZix0UoTCiLBehLlCgN12cm0-wl3q7ATjTc0ziDO8Q/edit",
+		type: "NJPW"
+	},
+	{
+		id:5,
+		name: "IMPACT 2018",
+		date: new Date(),
+		year: null,
+		driveLink:"https://docs.google.com/document/d/1MmrjdV7u3C889Q208JQRAs2KPhG8u1wLEkX2xGdqdZQ/edit",
+		type: "IMPACT"
+	},
+	{
+		id:6,
+		name: "Jap 2018 : AJPW NOAH Dragon Gate DDT BJW",
+		date: new Date(),
+		year: null,
+		driveLink:"https://docs.google.com/document/d/1RuTip2OduveqHkmTTGe-pr1sa3hbUYDhdvqDkZcfBRY/edit",
+		type: "4starslist"
+	},
+	{
+		id:7,
+		name: "PROGRESS 2018",
+		date: new Date(),
+		year: null,
+		driveLink:"https://docs.google.com/document/d/1xyJzOJQbHiaBjcT3-Q3oOthE5gJuLkmy9dcd25yS3Do/edit",
+		type: "PROGRESS"
+	},
+	{
+		id:8,
+		name: "ROH 2018",
+		date: new Date(),
+		year: null,
+		driveLink:"https://docs.google.com/document/d/1uFLYDA4G-CXz-qjODfCgZ_KE5b8XmUlvI07QhVenKgs/edit",
+		type: "ROH"
+	},
+	{
+		id:9,
+		name: "PWG 2018",
+		date: new Date(),
+		year: null,
+		driveLink:"https://docs.google.com/document/d/1dw0jrt6UCYQcK3qkt88IC7ZCdb_2ajtr-xG0Q8CeFvI/edit",
+		type: "PWG"
+	},
+	{
+		id:10,
+		name: "WWE NXT UK 2018",
+		date: new Date(),
+		year: null,
+		driveLink:"https://docs.google.com/document/d/11SAq9SWSwlNvcqWVV0FsnALhHe3fw4xCe4b2YYmRAY4/edit",
+		type: "WWE"
+	},
+	{
+		id:11,
+		name: "Indé 2018 wXw EVOLVE GCW DEFIANT IWA ALL IN",
+		date: new Date(),
+		year: null,
+		driveLink:"https://docs.google.com/document/d/1ZjOGVs-pq62lqF-67taJqPyCG152RGPKMqJ7GFE0WXE/edit",
+		type: "4starslist"
+	},
+	{
+		id:11,
+		name: "Bilan mi parcours 2018",
+		date: new Date(),
+		year: null,
+		driveLink:"https://docs.google.com/document/d/14KMNmUIqUn0-dsLLB5EcKfumtN-S5ZPgllEBpoggG-s/edit?usp=sharing",
 		type: "4starslist"
 	},
 ];
