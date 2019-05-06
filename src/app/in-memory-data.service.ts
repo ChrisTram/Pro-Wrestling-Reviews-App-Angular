bd8e1568fcb2c48c4d38b472941c63623c6469f2
Unknown <chris.tramier97@gmail.com>
2019-05-06 14:06:51 +0200

    Removed old pokemon mock (used for tests)

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,12 +1,10 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-import { POKEMONS } from './pokemons/mock-pokemon';
 import { REVIEWS } from './wrestling/mock-review';
 //utilisation du mock à nouveau, le node js c'est bien mais pas sur un serveur mutualisé...
 
 export class InMemoryDataService implements InMemoryDbService {
 	createDb() {
-		let pokemons = POKEMONS;
 		let reviews = REVIEWS;
-		return { pokemons, reviews };
+		return reviews;
 	}
 }
