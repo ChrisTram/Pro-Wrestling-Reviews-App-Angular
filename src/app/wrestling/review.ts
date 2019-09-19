6983dbdc3cfb7eb62b1f3addbb4040785436ccd6
Unknown <chris.tramier97@gmail.com>
2019-09-19 16:21:07 +0200

    minor text edits

--- src/app/wrestling/review.ts
+++ src/app/wrestling/review.ts
@@ -8,22 +8,3 @@ export class Review {
                   //ex : Jap, PWG, UK, WWE, WWE NXT, WWE 205 Live, PROGRESS...
     types: Array<string>;
   }
-
-
-  //Inutilisé pour le moment, à voir comment je recup toutes mes reviews (trop pour ranger dans des db...)
-  /*
-  export class Event extends Review {
-    id: number;
-    name: string;
-    picture: string;
-    matchs: Match[];
-    date: Date;
-  }
-
-  export class Match extends Review {
-    id: number;
-    name: string;
-    picture: string;
-    date: Date;
-    event: Event;
-  }*/
